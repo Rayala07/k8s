465ba44f6959179dbf4edd2ac3936ce3b75ad10d
Rayala07 <rayala07viswanath@gmail.com>
2026-05-29 15:45:33 +0530

    Server setup + Docker build

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express"
+import morgan from "morgan"
+
+const app = express();
+
+app.use(morgan())
+
+app.get("/", (req, res) => {
+    let sum = 0;
+
+    for (let i = 0; i < 1000000000; i++) {
+        sum += i;
+    }
+
+    res.send(`Hello, the sum is ${sum}`)
+})
+
+app.listen(3000, () => {
+    console.log("Server running on PORT: 3000")
+})
